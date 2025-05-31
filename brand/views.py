fb2c54dbdcbfe0fc4daa73ac64b3a86b0e90c0cf
Temberland <teslay.123@gmail.com>
2025-05-31 17:17:07 +1000

    add create and delete method in brand

--- brand/views.py
+++ brand/views.py
@@ -6,11 +6,13 @@ import json
 from django.http import JsonResponse
 from .models import Brand
 from brand.models import Brand
-from category.models import category
+
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+# Получение всех брендов
 def get_all_brands(request):
     brands = Brand.objects.all()
     data = []
@@ -22,3 +24,39 @@ def get_all_brands(request):
             "description": brand.description
         })
     return JsonResponse(data, safe=False)
+
+
+# Создание бренда
+@csrf_exempt
+def create_brand(request):
+    if request.method == "POST":
+        try:
+            brand = Brand.objects.create(
+                name=request.POST["name"],
+                description=request.POST["description"],
+            )
+            return JsonResponse({'message': 'сreation is successful'}, status=201)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    return JsonResponse({"error": 'only method POST'}, status=405)
+
+
+# Обновление бренда
+@csrf_exempt
+def update_brand(request, id):
+    pass
+
+
+# удаление бренда
+@csrf_exempt
+def delete_brand(request, id):
+    try:
+        if request.method == "DELETE":
+            brand = Brand.objects.get(id=id)
+            brand.delete()
+            return JsonResponse({'message': 'brand removed'})
+    except Brand.DoesNotExist:
+        return JsonResponse({"error": "brand is not found"}, status=404)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=405)
