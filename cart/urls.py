97e7f82d9c0b77244e1ea67a542dc3e56ef796bd
Temberland <teslay.123@gmail.com>
2025-06-14 07:40:20 +1000

    add method clear

--- cart/urls.py
+++ cart/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from django.contrib import admin
 from .views import *
 
-
 # url пока не готова авторизация, для теста
 urlpatterns = [
     path('get_cart/', get_cart, name='get_cart'),
     path('add_to_cart/', add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
     path('update_cart_item_quantity/', update_cart_item_quantity, name='update_cart_item_quantity'),
+    path('clear_cart/', clear_cart, name='clear_cart'),
 ]
